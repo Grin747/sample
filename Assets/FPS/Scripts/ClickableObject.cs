dfd3adb3b1e7276c03164b63c5591bae7b9f580b
Grin <grinlikegreenbutgrin@gmail.com>
2020-12-23 20:59:05 +0500

    late init

---
+++ Assets/FPS/Scripts/ClickableObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace FPS.Scripts
+{
+    public class ClickableObject : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject action;
+
+        private void OnMouseEnter()
+        {
+            action.SetActive(true);
+        }
+
+        private void OnMouseExit()
+        {
+            action.SetActive(false);
+        }
+    }
+}
---
+++ Assets/FPS/Scripts/UI/CrosshairManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairManager : MonoBehaviour
+{
+    public Image crosshairImage;
+    public Sprite nullCrosshairSprite;
+    public float crosshairUpdateshrpness = 5f;
+
+    bool m_WasPointingAtEnemy;
+    RectTransform m_CrosshairRectTransform;
+    CrosshairData m_CrosshairDataDefault;
+    CrosshairData m_CrosshairDataTarget;
+    CrosshairData m_CurrentCrosshair;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+}
